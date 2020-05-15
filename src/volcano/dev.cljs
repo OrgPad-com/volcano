3cc805602d90deb5881c3b6e470b64b29e5c834a
Pavel Klavik <pavel.klavik@gmail.com>
2020-05-15 15:39:54 +0200

    Sequence made not lazy for Reagent.

--- src/volcano/dev.cljs
+++ src/volcano/dev.cljs
@@ -21,12 +21,12 @@
                  :justify-content  "center"
                  :width            "100%"
                  :flex-wrap        "wrap"}}
-   (for [page-id (sort (keys pages))]
-     [:div {:key   page-id
-            :style {:padding     5
-                    :font-weight (when (= page-id @current-route) "bold")}}
-      [:a {:href  (b/path-for routes page-id)
-           :style {:color "white"}} (str page-id)]])])
+   (doall (for [page-id (sort (keys pages))]
+            [:div {:key   page-id
+                   :style {:padding     5
+                           :font-weight (when (= page-id @current-route) "bold")}}
+             [:a {:href  (b/path-for routes page-id)
+                  :style {:color "white"}} (str page-id)]]))])
 
 (defn render
   "Renders the page for the current route."
